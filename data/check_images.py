b4047acf723810e06c176740f26602a73c412be6
varsanyim <6471221+varsanyim@users.noreply.github.com>
2025-02-24 02:48:28 +0100

    TODO 0

--- data/check_images.py
+++ data/check_images.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/check_images.py
 #
-# TODO 0: Add your information below for Programmer & Date Created.                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
+# (y) TODO 0: Add your information below for Programmer & Date Created.                                                                             
+# PROGRAMMER: Wille, Krisztina Martina
+# DATE CREATED: 2025.02.23                                 
 # REVISED DATE: 
 # PURPOSE: Classifies pet images using a pretrained CNN model, compares these
 #          classifications to the true identity of the pets in the images, and
@@ -39,8 +39,8 @@ from print_results import print_results
 
 # Main program function defined below
 def main():
-    # TODO 0: Measures total program runtime by collecting start time
-    start_time = 
+    # (y) TODO 0: Measures total program runtime by collecting start time
+    start_time = time()
     
     # TODO 1: Define get_input_args function within the file get_input_args.py
     # This function retrieves 3 Command Line Arugments from user as input from
@@ -115,11 +115,11 @@ def main():
     # and incorrectly classified breeds (if requested)
     print_results(results, results_stats, None, True, True)
     
-    # TODO 0: Measure total program runtime by collecting end time
-    end_time =
+    # (y) TODO 0: Measure total program runtime by collecting end time
+    end_time = time()
     
-    # TODO 0: Computes overall runtime in seconds & prints it in hh:mm:ss format
-    tot_time = #calculate difference between end time and start time
+    # (y) TODO 0: Computes overall runtime in seconds & prints it in hh:mm:ss format
+    tot_time = end_time - start_time #calculate difference between end time and start time
     print("\n** Total Elapsed Runtime:",
           str(int((tot_time/3600)))+":"+str(int((tot_time%3600)/60))+":"
           +str(int((tot_time%3600)%60)) )
